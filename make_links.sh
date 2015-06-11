885c3424c109acd9de9e4935a7151b559adef2e8
Karen Cranston <karen.cranston@gmail.com>
2015-06-11 19:31:57 -0400

    add script to make symlinks'

---
+++ make_links.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+WKDIR="/Users/karen/Docuemnts/my_configs"
+
+ln -s $WKDIR/.vimrc $HOME
+ln -s $WKDIR/.gitignore_global $HOME
+ln -s $WKDIR/.zshrc $HOME
+ln -s $WKDIR/karen.zsh-theme $HOME/.oh-my-zsh/theme
